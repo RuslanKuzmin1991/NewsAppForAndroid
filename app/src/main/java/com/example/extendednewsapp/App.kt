d59bf0835586766a6556edc6484a32cfc01a2ffb
RuslanKuzmin1991 <ruslan.kuzmin.1991@gmail.com>
2024-11-19 22:29:26 +0100

    Implemented initial structure and libs

---
+++ app/src/main/java/com/example/extendednewsapp/App.kt
@@ -0,0 +1,13 @@
+package com.example.extendednewsapp
+
+import android.app.Application
+import org.koin.core.context.startKoin
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+        }
+    }
+}
